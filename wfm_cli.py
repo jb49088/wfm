cc9af3eecf562ac39947dbaaea34b9c89d5c501e
Justin Braun <justinbraun335@gmail.com>
2026-01-14 11:05:02 -0500

    chore: drop alt screen

--- wfm_cli.py
+++ wfm_cli.py
@@ -2,21 +2,9 @@
 # =                                   WFM_CLI                                    =
 # ================================================================================
 
-import sys
-
 from copy_user_listings import copy_user_listings
 from display_item_listings import display_item_listings
 from display_user_listings import display_user_listings
-
-
-def enter_alt_screen():
-    sys.stdout.write("\033[?1049h")
-    sys.stdout.flush()
-
-
-def exit_alt_screen():
-    sys.stdout.write("\033[?1049l")
-    sys.stdout.flush()
 
 
 def wfm_cli():
@@ -26,10 +14,7 @@ def wfm_cli():
 
 
 if __name__ == "__main__":
-    # enter_alt_screen()
     try:
         wfm_cli()
     except KeyboardInterrupt:
         pass
-    # finally:
-    # exit_alt_screen()
