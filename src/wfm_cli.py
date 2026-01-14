f038baf27deb536fa30ca0fe04be746713dc7667
Justin Braun <justinbraun335@gmail.com>
2026-01-14 17:54:41 -0500

    feat: implement command history

--- src/wfm_cli.py
+++ src/wfm_cli.py
@@ -4,7 +4,8 @@
 
 import shlex
 
-from prompt_toolkit import ANSI, prompt
+from prompt_toolkit import ANSI, PromptSession
+from prompt_toolkit.history import FileHistory
 
 from copy_user_listings import copy_user_listings
 from display_item_listings import display_item_listings
@@ -13,10 +14,11 @@ from utils import clear_screen
 
 
 def wfm_cli():
+    session = PromptSession(history=FileHistory("data/history"))
     status = "\033[32mIn Game\033[0m"
     while True:
         try:
-            cmd = prompt(ANSI(f"wfm_cli [{status}]> ")).strip()
+            cmd = session.prompt(ANSI(f"wfm_cli [{status}]> ")).strip()
         except KeyboardInterrupt:
             break
 
